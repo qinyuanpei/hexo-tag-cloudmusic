87169caee19890f06e93fb87b8295012e6d2c341
qinyuanpei <qinyuanpei@163.com>
2015-11-09 10:48:56 +0800

    update the README.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # hexo-tag-cloudmusic
-just a hexo tag plugin with cloudmusic
+This is a hexo tag plugin for cloudmusic
+
+## Introduction
+
+This is a hexo tag plugin for cloudmusic
+
+
+## Installation
+
+To install, run the following command in the root directory of hexo:
+```
+npm install hexo-tag-cloudmusic --save
+```
+
+To config if you nedd in the __config.yml of Hexo:
+```
+# CloudMusic
+widgetType:  flash ## It can be flash or iframe
+widgetSize:  small ## It can be small or big or custom if widgetType is iframe
+autoPlay:  0 ## It can be 0 to set autoPlay disable or 1 to set autoPlay enable
+width: 278　## Set the widget width if the widgetType is irfame and the widgetSize is custom
+height: 32　##　Set the widget height if the widgetType is irfame and the widgetSize is custom
+```
+
+## Usage
+```
+{% cloudmusic 20744788 %}
+```
+
+
---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+* hexo-tag-cloudmusic
+* https://github.com/Z4Tech/hexo-tag-bilibili.git
+* Copyright (c) 2015, Z4Tech
+* Licensed under the MIT license.
+* Syntax:
+* {% bilibili [av_id] %} or {% bilibili [av_id] [page] %}
+*/
+
+hexo.extend.tag.register('cloudmusic', function(args){
+  var sid = args[0];
+  var config = hexo.config.cloudmusic || {};
+  var widgetType = hexo.config.widgetType || 'flash';
+  var widgetSize = config.widgetSize || 'small';
+  var autoPlay = config.autoPlay || 1;
+  var width = config.width || 278;
+  var height = config.height || 32;
+
+  if(widgetType == 'iframe'){
+  	if(widgetSize=='small'){
+  		return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="http://music.163.com/outchain/player?type=2&id=' + sid + '&auto=' + autoPlay +'&height=32"></iframe>';
+  	}else if(widgetSize=='big'){
+  		return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=351 height=86 src="http://music.163.com/outchain/player?type=2&id=' + sid + '&auto=' + autoPlay + '&height=66"></iframe>';
+  	}else if(widgetSize=='custom'){
+  		return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=' + width +' height=' + height +' src="http://music.163.com/outchain/player?type=2&id=' + sid + '&auto=' + autoPlay + '&height=66"></iframe>';
+  	}
+  }else{
+  	if(widgetSize=='small'){
+  		return '<embed src="http://music.163.com/style/swf/widget.swf?sid=' + sid + '&type=2&auto=' + autoPlay + '&width=278&height=32" width="298" height="52"  allowNetworking="all"></embed>';
+  	}else{
+  		return '<embed src="http://music.163.com/style/swf/widget.swf?sid=' + sid + '&type=2&auto=' + autoPlay + '&width=320&height=66" width="340" height="86"  allowNetworking="all"></embed>';
+  	}
+  }
+});
